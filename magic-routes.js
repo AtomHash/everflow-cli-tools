fbead5728703a77e7c8cf0024baec16a660d2679
Dylan Harty <me@dylanharty.com>
2020-05-04 18:52:04 -0600

    router/routes.ts is the new location for generated file

--- magic-routes.js
+++ magic-routes.js
@@ -4,22 +4,23 @@ var fs = require('fs-extra');
 // Generate Module routes.ts
 modulesPath = "./src/modules";
 moduleRoutes = path.join(path.resolve(modulesPath));
-routesFile = './src/routes.ts';
+routesFile = './src/router/routes.ts';
 routesFileText = "";
 var importNames = [];
 var stream = fs.createWriteStream(routesFile);
+stream.write("import { RouteConfig } from 'vue-router';\n");
 for (let folder of fs.readdirSync(path.resolve(modulesPath)))
 {
     if (fs.statSync(path.join(path.resolve(modulesPath), folder)).isDirectory())
     {
         moduleRoutesName = folder + 'Routes';
         importNames.push(moduleRoutesName);
-        stream.write("import " + moduleRoutesName + " from \"./modules/" + folder + "/routes\";\n");
+        stream.write("import " + moduleRoutesName + " from \"../modules/" + folder + "/routes\";\n");
     }
 }
 counter = 0;
 if (importNames.length > 1){
-    stream.write("const routes: Array<any> = "+importNames[0]+".concat(");
+    stream.write("const routes: Array<RouteConfig> = "+importNames[0]+".concat(");
     for (let importName of importNames)
 {
     counter++;
@@ -36,7 +37,7 @@ if (importNames.length > 1){
 }
 stream.write(");\n");
 } else{
-    stream.write("const routes: Array<any> = "+importNames[0]+";");
+    stream.write("const routes: Array<RouteConfig> = "+importNames[0]+";");
 }
-stream.write("export default routes;");
+stream.write("\nexport default routes;");
 stream.end();
