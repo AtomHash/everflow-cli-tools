90f01e454e7981a09c6e863d78b93f384d09fe75
Dylan Harty <me@dylanharty.com>
2020-05-06 13:39:15 -0600

    added store vuex support for generation

--- magic-routes.js
+++ magic-routes.js
@@ -1,43 +1,107 @@
 var path = require('path');
 var fs = require('fs-extra');
 
+function camelCase(str){
+  let arr = str.split('-');
+  let capital = arr.map((item, index) => index ? item.charAt(0).toUpperCase() + item.slice(1).toLowerCase() : item);
+  // ^-- change here.
+  let capitalString = capital.join("");
+  return capitalString;
+}
+
 // Generate Module routes.ts
 modulesPath = "./src/modules";
 moduleRoutes = path.join(path.resolve(modulesPath));
 routesFile = './src/router/routes.ts';
-routesFileText = "";
+storeFile = './src/store/index.ts';
 var importNames = [];
-var stream = fs.createWriteStream(routesFile);
-stream.write("import { RouteConfig } from 'vue-router';\n");
+var routesStream = fs.createWriteStream(routesFile);
+var storesStream = fs.createWriteStream(storeFile);
+routesStream.write("import { RouteConfig } from 'vue-router';\n");
+
+// Vue store setup
+storesStream.write("import Vue from 'vue';\n");
+storesStream.write("import Vuex from 'vuex';\n");
 for (let folder of fs.readdirSync(path.resolve(modulesPath)))
 {
     if (fs.statSync(path.join(path.resolve(modulesPath), folder)).isDirectory())
     {
-        moduleRoutesName = folder + 'Routes';
-        importNames.push(moduleRoutesName);
-        stream.write("import " + moduleRoutesName + " from \"../modules/" + folder + "/routes\";\n");
+        var importName = camelCase(folder);
+        importNames.push(importName);
+
+        // Routes write import
+        routesStream.write("import " + importName + 'Routes' + " from \"../modules/" + folder + "/routes\";\n");
+
+        //Store write import
+        storesStream.write("import " + importName + 'Store' + " from \"../modules/" + folder + "/store\";\n");
     }
 }
+
+// Finish vue vuex init
+storesStream.write("Vue.use(Vuex);\n\n");
+
 counter = 0;
-if (importNames.length > 1){
-    stream.write("const routes: Array<RouteConfig> = "+importNames[0]+".concat(");
-    for (let importName of importNames)
+
+//Handle Routes
+if (importNames.length > 1)
+{
+    routesStream.write("const routes: Array<RouteConfig> = "+importNames[0]+ "Routes" + ".concat(");
+}
+if (importNames.length == 1)
+{
+    routesStream.write("const routes: Array<RouteConfig> = "+importNames[0]+ "Routes;");
+}
+
+// Start vuex write
+storesStream.write("export default new Vuex.Store({\n");
+storesStream.write("modules: {\n");
+
+for (let importName of importNames)
 {
     counter++;
-    if (importName == importNames[0]) {
-        continue;
+
+    // Build route list
+    if (importNames.length > 1){
+        buildRoutes(importName + "Routes");
+    }
+
+    // Build store list
+    buildStores(importName);
+}
+
+// Finish routes file
+if (importNames.length > 1)
+{
+    routesStream.write(");\n");
+}
+routesStream.write("\nexport default routes;");
+routesStream.end();
+
+// Finish stores file
+storesStream.write("});");
+
+
+function buildRoutes(importName)
+{
+    if (importName == importNames[0]+ "Routes") {
+        return;
     }
     if (counter === importNames.length)
     {
-        stream.write(importName);
+        routesStream.write(importName);
     } else
     {
-        stream.write(importName + ",\n");
+        routesStream.write(importName + ",\n");
     }
 }
-stream.write(");\n");
-} else{
-    stream.write("const routes: Array<RouteConfig> = "+importNames[0]+";");
+
+function buildStores(importName)
+{
+    if (counter === importNames.length)
+    {
+        storesStream.write(importName + ": "+importName+ "Store}\n");
+    } else
+    {
+        storesStream.write(importName + ": "+importName+ "Store,\n");
+    }
 }
-stream.write("\nexport default routes;");
-stream.end();
