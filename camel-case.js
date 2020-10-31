09de07d22dd2de4211f99847b5645b4c3a18c7cb
Dylan Harty <me@dylanharty.com>
2020-10-30 23:32:26 -0600

    added new store locations, converted camel-case to js file module and updated package.json

---
+++ camel-case.js
@@ -0,0 +1,7 @@
+module.exports = function(str)
+{
+    let arr = str.split('-');
+    let capital = arr.map((item, index) => index ? item.charAt(0).toUpperCase() + item.slice(1).toLowerCase() : item);
+    let capitalString = capital.join("");
+    return capitalString;
+}
